f54af104cd5ae1ebf1d8da15f290e1252bc79e19
David-Tbo <davidtbo@outlook.fr>
2025-07-01 23:27:03 +0200

    conf.py review

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -57,5 +57,7 @@ exclude_patterns = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+import sphinx_rtd_theme
+
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
